008e3c232e930d9c3bc9f13af0dded602e1d35da
LuizFelipe <luizfelipe.freire@gmail.com>
2024-09-25 22:01:48 -0300

    Cap 02 Exemplo 2

--- livrojs/cap02/js/exe2_1.js
+++ livrojs/cap02/js/exe2_1.js
@@ -3,8 +3,10 @@ const frm = document.querySelector("form");
 const resp = document.querySelector("h3");
 
 // cria um 'ouvinte' de evento, acionado quando o botão submit for clicado
-frm.addEventListener("submit", (e)=>{
-    const nome = frm.inNome.value; // obtém o nome digitado no form
-    resp.innerText = `Olá ${nome}`; // exibe a respota do programa
-    e.preventDefault(); // vai evitar que o form envio os dados pro destino.php
-});
+frm.addEventListener("submit", executar);
+
+function executar(evento) {
+    const nome = frm.inNome.value; // obtém o nome digitado no form 
+    resp.innerHTML = `Olá <i>${nome}</i>`; // exibe a respota do programa
+    evento.preventDefault();
+}
