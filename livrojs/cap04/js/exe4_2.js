48c730ff2072c6f77d849cdb7de705d3f9fdd193
LuizFelipe <luizfelipe.freire@gmail.com>
2024-10-03 21:07:00 -0300

    Exemplo 4.3

--- livrojs/cap04/js/exe4_2.js
+++ livrojs/cap04/js/exe4_2.js
@@ -4,16 +4,21 @@ function calcular(){
     const masculino = document.getElementById("inMasculino").checked;
     const altura = document.getElementById("inAltura").value;
     
-    let peso;
+    let peso = masculino ? Math.pow(altura,2) * 22 : Math.pow(altura,2) * 21;
 
+    /*
     if(masculino){
         peso = 22 * Math.pow(altura, 2)
     }else{
         peso = 21 * Math.pow(altura, 2)
-    }
+    }*/
 
-    document.querySelector("h3").innerText = `Peso ideal ${peso.toFixed(2)}`;
+    document.querySelector("h3").innerText = `${nome}: Peso ideal ${peso.toFixed(2)}`;
 
     return false; // vai impedir o refresh da página equivalente ao e.preventDefault()
 
+}
+
+function limpar() {
+document.querySelector("h3").innerText = "";
 }
