63ebdc4a5f33fd7871f503004875126b11346966
LuizFelipe <luizfelipe.freire@gmail.com>
2024-10-30 19:58:38 -0300

    update script

--- bd/exercicio-campeonato-brasileiro/script.sql
+++ bd/exercicio-campeonato-brasileiro/script.sql
@@ -27,3 +27,37 @@ estado varchar(150),
 id_estadio int not null,
 constraint PK_Time primary key(id_time),
 constraint FK_EstadioTime foreign key(id_estadio) references estadio(id_estadio));
+
+create table jogador(
+id_jogador int,
+nome varchar(150),
+numero int,
+dt_nascimento date,
+posicao varchar(150),
+id_time int,
+constraint PK_Jogador primary key(id_jogador),
+constraint FK_JogadorTime foreign key(id_time) references time(id_time));
+
+create table partida(
+id_partida int,
+rodada int,
+horario datetime,
+gol_mandante int,
+gol_visitante int,
+id_mandante int,
+id_visitante int,
+id_estadio int,
+constraint PK_Partida primary key(id_partida),
+constraint FK_TimeMandante foreign key(id_mandante) references time(id_time),
+constraint FK_TimeVisitante foreign key(id_visitante) references time(id_time),
+constraint FK_PartidaEstadio foreign key(id_estadio) references estadio(id_estadio));
+
+create table evento(
+id_evento int,
+minuto int,
+descricao varchar(150),
+id_partida int,
+id_jogador int,
+constraint PK_Evento primary key(id_evento),
+constraint FK_EventoPartida foreign key(id_partida) references partida(id_partida),
+constraint FK_EventoJogador foreign key(id_jogador) references jogador(id_jogador));
