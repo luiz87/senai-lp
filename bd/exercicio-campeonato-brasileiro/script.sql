d422a9fb7eb47d680626f6d00c797be52cf581d2
LuizFelipe <luizfelipe.freire@gmail.com>
2024-10-29 21:01:51 -0300

    update script

--- bd/exercicio-campeonato-brasileiro/script.sql
+++ bd/exercicio-campeonato-brasileiro/script.sql
@@ -16,3 +16,14 @@ capacidade int);
 show tables; -- mostra as tabelas de uma base de dados
 
 desc estadio; -- descreve a estrutura de uma tabela
+
+create table time(
+id_time int,
+nome_completo varchar(150),
+nome varchar(150),
+sigla varchar(3),
+cidade varchar(150),
+estado varchar(150),
+id_estadio int not null,
+constraint PK_Time primary key(id_time),
+constraint FK_EstadioTime foreign key(id_estadio) references estadio(id_estadio));
