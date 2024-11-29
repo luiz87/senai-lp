86ba84165097d3318fc75f07c94203172f261367
LuizFelipe <luizfelipe.freire@gmail.com>
2024-11-28 21:47:24 -0300

    resolução até a questão 48

--- bd/lista-querys-dblocadora-resolucoes.sql
+++ bd/lista-querys-dblocadora-resolucoes.sql
@@ -235,31 +235,151 @@ que inicie com a Letra “E”?*/
 
 /*35. Quais os países que possuem mais de 3 cidades que iniciam com a Letra “A”?*/
 
-/*36. Quais os países que possuem mais de 3 cidades que iniciam com a Letra “A” ou tenha "R" ordenando por quantidade crescente?*/
+    select count(*) as qt_cidades, p.pais from pais as p 
+    inner join cidade as c on p.pais_id = c.pais_id
+    where cidade like 'A%'
+    group by p.pais
+    having qt_cidades > 3
+    order by qt_cidades desc;
+
+/*36. Quais os países que possuem mais de 3 cidades 
+que iniciam com a Letra “A” ou tenha "R" ordenando por quantidade crescente?*/
+
+    select count(*) as qt_cidades, p.pais from pais as p 
+    inner join cidade as c on p.pais_id = c.pais_id
+    where cidade like 'A%' or cidade like '%r%'
+    group by p.pais
+    having qt_cidades > 3
+    order by qt_cidades asc;    
 
 /*37. Quais os clientes moram no país “United States”?*/
 
+    select 
+        count(*) over(partition by p.pais) qt, 
+        row_number() over(partition by p.pais) num, 
+        concat_ws(' ', primeiro_nome, ultimo_nome) as nome_completo,
+        p.pais
+    from cliente as cli
+    inner join endereco as e on cli.endereco_id = e.endereco_id
+    inner join cidade as c on e.cidade_id = c.cidade_id
+    inner join pais as p on c.pais_id = p.pais_id
+    where p.pais = 'United States';
+    
 /*38. Quantos clientes moram no país “Brazil”?*/
 
+    select 
+        count(*) qt 
+    from cliente where endereco_id in (
+    select endereco_id from endereco where cidade_id in (
+    select cidade_id from cidade where pais_id in (
+    select pais_id from pais where pais in ('Brazil'))));
+    
 /*39. Qual a quantidade de clientes por pais?*/
+
+    select 
+        distinct 
+        count(*) over(partition by p.pais) qt, 
+        p.pais
+    from cliente as cli
+    inner join endereco as e on cli.endereco_id = e.endereco_id
+    inner join cidade as c on e.cidade_id = c.cidade_id
+    inner join pais as p on c.pais_id = p.pais_id
+    order by p.pais;
 
 /*40. Quais países possuem mais de 10 clientes?*/
 
+    select         
+        p.pais,
+        count(*) qt_cidade,
+        group_concat(c.cidade) cidades
+    from cliente as cli
+    inner join endereco as e on cli.endereco_id = e.endereco_id
+    inner join cidade as c on e.cidade_id = c.cidade_id
+    inner join pais as p on c.pais_id = p.pais_id
+    group by p.pais
+    having qt_cidade > 10
+    order by p.pais;
+
 /*41. Qual a média de duração dos filmes por idioma?*/
 
-/*42. Qual a quantidade de atores que atuaram nos filmes do idioma “English”?*/
+    select i.nome, avg(duracao_do_filme) media from filme as f 
+    inner join idioma as i on f.idioma_id = i.idioma_id
+    group by i.nome;
 
+/*42. Qual a quantidade de atores que atuaram nos filmes do idioma “English”?*/
+    
+    select count(distinct ator_id) from filme as f 
+    inner join idioma as i on f.idioma_id = i.idioma_id
+    inner join filme_ator as fa on f.filme_id = fa.filme_id
+    where i.nome = 'English';
+    
 /*43. Quais os atores do filme “BLANKET BEVERLY”?*/
 
-/*44. Quais categorias possuem mais de 60 filmes cadastrados?*/
+    select f.titulo, a.primeiro_nome, a.ultimo_nome from filme as f
+    inner join filme_ator as fa on f.filme_id = fa.filme_id
+    inner join ator as a on fa.ator_id = a.ator_id
+    where f.titulo = 'BLANKET BEVERLY'
+    order by a.primeiro_nome;
 
+/*44. Quais categorias possuem mais de 60 filmes cadastrados?*/
+    
+    select c.nome , count(*) quantidade from filme as f 
+    inner join filme_categoria as fc on f.filme_id = fc.filme_id
+    inner join categoria as c on fc.categoria_id = c.categoria_id
+    group by c.nome
+    having quantidade > 60;
+    
 /*45. Quais os filmes alugados (sem repetição) para clientes que moram na “Argentina”?*/
+/*
+    Instrução SQL CREATE VIEW
+    Em SQL, uma exibição é uma tabela virtual baseada no conjunto de resultados de uma instrução SQL.
+    Uma view contém linhas e colunas, assim como uma tabela real. 
+    Os campos em uma view são campos de uma ou mais tabelas reais no banco de dados.
+    Você pode adicionar instruções e funções SQL a uma exibição e apresentar os dados como se eles 
+    estivessem vindo de uma única tabela.
+    Uma visualização é criada com a CREATE VIEW
+*/
+    create or replace view filme_cliente_pais as (
+    select f.titulo, p.pais from pais as p
+    inner join cidade as c on p.pais_id = c.pais_id
+    inner join endereco as e on c.cidade_id = e.cidade_id
+    inner join cliente as cli on e.endereco_id = cli.endereco_id
+    inner join aluguel as a on cli.cliente_id = a.cliente_id
+    inner join inventario as i on i.inventario_id = a.inventario_id
+    inner join filme as f on f.filme_id = i.filme_id
+    order by f.titulo);
+    
+    select distinct titulo from filme_cliente_pais
+    where pais = 'Argentina';
 
 /*46. Qual a quantidade de filmes alugados por Clientes que moram na “Chile”?*/
 
+    select count(*) qt_aluguel, count(distinct titulo) qt_filme
+    from filme_cliente_pais
+    where pais = 'Chile';
+
 /*47. Qual a quantidade de filmes alugados por funcionario?*/
 
+    select * from funcionario;
+    select * from aluguel;
+    select * from inventario;
+    
+    select f.primeiro_nome, f.ultimo_nome , count(*) quantidade
+    from funcionario as f 
+    inner join aluguel as a on f.funcionario_id = a.funcionario_id
+    inner join inventario as i on i.inventario_id = a.inventario_id
+    group by f.primeiro_nome, f.ultimo_nome;
+
 /*48. Qual a quantidade de filmes alugados por funcionario para cada categoria?*/
+
+    select c.nome, fu.primeiro_nome, fu.ultimo_nome, count(*) quantidade
+    from funcionario as fu
+    inner join aluguel as a on fu.funcionario_id = a.funcionario_id
+    inner join inventario as i on i.inventario_id = a.inventario_id
+    inner join filme as f on f.filme_id = i.filme_id
+    inner join filme_categoria as fc on f.filme_id = fc.filme_id
+    inner join categoria as c on fc.categoria_id = c.categoria_id
+    group by c.nome, fu.primeiro_nome, fu.ultimo_nome;
 
 /*49. Quais Filmes possuem preço da Locação maior que a média de preço da locação?*/
 
@@ -267,8 +387,8 @@ que inicie com a Letra “E”?*/
 
 /*51. Qual a quantidade de filmes locados mês a mês por ano? */
 
-/*52. Qual o total pago por classificação de filmes alugados no ano de 2006?*/
+/*52. Qual o total pago por classificação de filmes alugados no ano de 2005?*/
 
 /*53. Qual a média mensal do valor pago por filmes alugados no ano de 2005?*/
 
-/*54. Qual o total pago por filme alugado no mês de Junho de 2006 por cliente? -----    CORRIGIR  */
+/*54. Qual o total pago por filme alugado no mês de Junho de 2005 por cliente? -----    CORRIGIR  */
