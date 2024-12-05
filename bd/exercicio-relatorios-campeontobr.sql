0ef53599c916ec78b093d22600c60772541fb470
LuizFelipe <luizfelipe.freire@gmail.com>
2024-12-05 19:47:37 -0300

    add exercício

---
+++ bd/exercicio-relatorios-campeontobr.sql
@@ -0,0 +1,16 @@
+-- 01. Atualize na tabela evento as descrições 
+-- DE "Substitution" PARA "Substituição"
+-- DE "Woodwork" PARA "Bola na Trave"
+-- DE "Cartão Vermelho (Second Yellow Card)" PARA "Cartão Vermelho (Segundo Cartão Amarelo)"
+
+-- 02. Atualize o resultado das partidas do brasileirão 
+-- que já aconteceram e ainda não foi gravado o resultado.
+
+-- 03. Elabore um relatório por minuto e a quantidade de gols (não contar "Gol anulado (Var)")
+-- e ordene pela quantidade do maior para o menor
+
+-- 04. Elabore um relatório por idade e quantidade de jogadores
+
+-- 05. Elabore um relatório por jogador e quantidade de cartões, 
+-- detalhar também a quantidade de Cartões Vermelho e Amarelo
+-- ordene pela quantidade total de Cartões'
